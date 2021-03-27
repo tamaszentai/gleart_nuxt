ef53ca4a63cd4c11df8b03726da69abdab3880ef
Tamas Zentai <zentaitamas@gmail.com>
2021-03-27 13:41:01 +0000

    server removed

--- server/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a22d83c763833c2b935f3187bdea6738'),
-    },
-  },
-});
--- server/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'fcb9148d-4b11-4363-8411-10e223db4e4a'
-};
